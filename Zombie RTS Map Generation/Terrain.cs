9dfa47329842a9bc118c2d468a5bccafa797be12
Robert Hardy <helloarchy@gmail.com>
2020-06-20 19:34:17 +0100

    Add loops to generate the vertex array

--- Zombie RTS Map Generation/Terrain.cs
+++ Zombie RTS Map Generation/Terrain.cs
@@ -1,15 +1,31 @@
 using Godot;
 using System;
+using System.Linq;
+using Godot.Collections;
 
 public class Terrain : Spatial
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private int _width;
+    private int _height;
+    private Array<Vector3> _vertices = new Array<Vector3>();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        _width = 300;
+        _height = 300;
+
+        /* Add all triangle positions to the vertex array */
+        for (var x = 0; x < _width; x++)
+        {
+            for (var y = 0; y < _height; y++)
+            {
+                _vertices.Add(new Vector3(x, 0, -y));
+                _vertices.Add(new Vector3(x+1, 0, -y-1));
+                _vertices.Add(new Vector3(x+1, 0, -y));
+            }
+        }
+        
         
     }
 
