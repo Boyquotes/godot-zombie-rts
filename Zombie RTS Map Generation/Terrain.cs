78c4c42c24a7e690d1d9cce9f9dd18942ae42c26
Robert Hardy <helloarchy@gmail.com>
2020-06-20 20:12:26 +0100

    Mesh generating using surface tools, half of triangles missing.

--- Zombie RTS Map Generation/Terrain.cs
+++ Zombie RTS Map Generation/Terrain.cs
@@ -8,6 +8,7 @@ public class Terrain : Spatial
     private int _width;
     private int _height;
     private Array<Vector3> _vertices = new Array<Vector3>();
+    private ArrayMesh _temporaryMesh = new ArrayMesh();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -26,7 +27,18 @@ public class Terrain : Spatial
             }
         }
         
-        
+        // Use surface tool to generate the mesh
+        var surfaceTool = new SurfaceTool();
+        surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
+
+        foreach (var vertex in _vertices)
+        {
+            surfaceTool.AddVertex(vertex);
+        }
+
+        surfaceTool.Commit(_temporaryMesh);
+        GetNode<MeshInstance>("MeshInstance").Mesh = _temporaryMesh;
+
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
