33e484ea10c58208b9fb17d7c9fa5369afd5714b
Robert Hardy <helloarchy@gmail.com>
2020-06-20 11:11:47 +0100

    Refactor code - rearrange methods into logical order. Move all classes to scripts namespace. Tidy code and neaten to reduce nesting.

--- Zombie RTS 3D/Scripts/SelectionBox.cs
+++ Zombie RTS 3D/Scripts/SelectionBox.cs
@@ -1,61 +1,63 @@
 using Godot;
-using System;
 
-public class SelectionBox : Control
+namespace ZombieRTS.Scripts
 {
-    public Vector2 MousePosition = new Vector2();
-    public Vector2 StartSelectionPosition = new Vector2();
-    private static readonly Color SelectionBoxColour = new Color(0, 1, 0);
-    private const int SelectionBoxLineWidth = 3;
-
-    /**
-     * Called when the node enters the scene tree for the first time.
-     */
-    public override void _Ready()
+    public class SelectionBox : Control
     {
-    }
+        public Vector2 MousePosition = new Vector2();
+        public Vector2 StartSelectionPosition = new Vector2();
+        private static readonly Color SelectionBoxColour = new Color(0, 1, 0);
+        private const int SelectionBoxLineWidth = 3;
+
+        /**
+         * Called when the node enters the scene tree for the first time.
+         */
+        public override void _Ready()
+        {
+        }
 
 
-    /**
-     * Called every frame. 'delta' is the elapsed time since the previous frame.
-     */
-    public override void _Process(float delta)
-    {
-        Update(); // Draw changes
-    }
+        /**
+         * Called every frame. 'delta' is the elapsed time since the previous frame.
+         */
+        public override void _Process(float delta)
+        {
+            Update(); // Draw changes
+        }
 
 
-    /**
-     * Drawn on update
-     */
-    public override void _Draw()
-    {
-        // If visible and mouse moved, draw four lines to make selection box
-        if (!Visible || StartSelectionPosition == MousePosition) return; // Do nothing
-        
-        // Top line
-        DrawLine(
-            StartSelectionPosition,
-            new Vector2(MousePosition.x, StartSelectionPosition.y),
-            SelectionBoxColour, SelectionBoxLineWidth
-        );
-        // Left line
-        DrawLine(
-            StartSelectionPosition,
-            new Vector2(StartSelectionPosition.x, MousePosition.y),
-            SelectionBoxColour, SelectionBoxLineWidth
-        );
-        // Right line
-        DrawLine(
-            MousePosition,
-            new Vector2(MousePosition.x, StartSelectionPosition.y),
-            SelectionBoxColour, SelectionBoxLineWidth
-        );
-        // Bottom line
-        DrawLine(
-            MousePosition,
-            new Vector2(StartSelectionPosition.x, MousePosition.y),
-            SelectionBoxColour, SelectionBoxLineWidth
-        );
+        /**
+         * Drawn on update
+         */
+        public override void _Draw()
+        {
+            // If visible and mouse moved, draw four lines to make selection box
+            if (!Visible || StartSelectionPosition == MousePosition) return; // Do nothing
+
+            // Top line
+            DrawLine(
+                StartSelectionPosition,
+                new Vector2(MousePosition.x, StartSelectionPosition.y),
+                SelectionBoxColour, SelectionBoxLineWidth
+            );
+            // Left line
+            DrawLine(
+                StartSelectionPosition,
+                new Vector2(StartSelectionPosition.x, MousePosition.y),
+                SelectionBoxColour, SelectionBoxLineWidth
+            );
+            // Right line
+            DrawLine(
+                MousePosition,
+                new Vector2(MousePosition.x, StartSelectionPosition.y),
+                SelectionBoxColour, SelectionBoxLineWidth
+            );
+            // Bottom line
+            DrawLine(
+                MousePosition,
+                new Vector2(StartSelectionPosition.x, MousePosition.y),
+                SelectionBoxColour, SelectionBoxLineWidth
+            );
+        }
     }
 }
