1b5220b5722deb41f26cf1e09761ea2d504e44f8
Robert Hardy <helloarchy@gmail.com>
2020-06-21 15:13:35 +0100

    Files moved to respective directories. All shadows now generate giving an entirely flat terrain.

---
+++ Zombie RTS Map Generation/Scripts/Terrain.cs
@@ -0,0 +1,61 @@
+using Godot;
+using Godot.Collections;
+
+namespace ZombieRTSMapGeneration.Scripts
+{
+    public class Terrain : Spatial
+    {
+        private int _width;
+        private int _height;
+        private Array<Vector3> _vertices;
+        private ArrayMesh _temporaryMesh;
+
+        /**
+     * Called when the node enters the scene tree for the first time.
+     */
+        public override void _Ready()
+        {
+            _width = 300;
+            _height = 300;
+            _vertices = new Array<Vector3>();
+            _temporaryMesh = new ArrayMesh();
+
+            /* Add all triangle positions to the vertex array */
+            for (var x = 0; x < _width; x++)
+            {
+                for (var y = 0; y < _height; y++)
+                {
+                    _vertices.Add(new Vector3(x, 0, -y));
+                    _vertices.Add(new Vector3(x, 0, -y-1));
+                    _vertices.Add(new Vector3(x+1, 0, -y-1));
+                    
+                    _vertices.Add(new Vector3(x, 0, -y));
+                    _vertices.Add(new Vector3(x+1, 0, -y-1));
+                    _vertices.Add(new Vector3(x+1, 0, -y));
+                }
+            }
+        
+            // Use surface tool to generate the mesh
+            var surfaceTool = new SurfaceTool();
+            surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
+            surfaceTool.SetMaterial(ResourceLoader.Load<Material>("res://Materials/Terrain.tres"));
+
+            foreach (var vertex in _vertices)
+            {
+                surfaceTool.AddVertex(vertex);
+            }
+
+            surfaceTool.Commit(_temporaryMesh);
+            GetNode<MeshInstance>("MeshInstance").Mesh = _temporaryMesh;
+        }
+
+    
+        /**
+     * Called every frame. 'delta' is the elapsed time since the previous frame.
+     */
+        public override void _Process(float delta)
+        {
+            
+        }
+    }
+}
