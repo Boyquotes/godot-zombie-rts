80576195153ca32135c4aecbc764ef191e874197
Robert Hardy <helloarchy@gmail.com>
2020-06-28 13:54:35 +0100

    update

--- Zombie RTS 3D Shader Maps/TerrainMesh.cs
+++ Zombie RTS 3D Shader Maps/TerrainMesh.cs
@@ -1,32 +1,33 @@
 using Godot;
-using System;
 
-public class TerrainMesh : MeshInstance
+namespace ZombieRTS3DShaderMaps
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
+    public class TerrainMesh : MeshInstance
     {
-        // TODO: Continue here https://docs.godotengine.org/en/stable/tutorials/shading/your_first_shader/your_second_spatial_shader.html
-        
-        
-        var derp = Mesh.SurfaceGetMaterial(0) as ShaderMaterial;
-        if (derp != null)
+        // Declare member variables here. Examples:
+        // private int a = 2;
+        // private string b = "text";
+
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
         {
-            derp.SetShaderParam("height_scale", 2.5);
+            // TODO: Continue here https://docs.godotengine.org/en/stable/tutorials/shading/your_first_shader/your_second_spatial_shader.html
+
+
+            if (Mesh.SurfaceGetMaterial(0) is ShaderMaterial shaderMaterial)
+            {
+                shaderMaterial.SetShaderParam("height_scale", 5.5);
+            }
+            else
+            {
+                GD.Print("Not shader material");
+            }
         }
-        else
-        {
-            GD.Print("Not shader material");
-        }
-    }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
 //  public override void _Process(float delta)
 //  {
 //      
 //  }
+    }
 }
