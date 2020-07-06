f6772781b0b75a1f935dc079b115c674775aa329
Robert Hardy <helloarchy@gmail.com>
2020-07-06 21:19:22 +0100

    Basic camera movement added via CameraController.cs

---
+++ Unity/Zombie RTS 3D/Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panSpeed = 20f; // Units per second of movement
+
+    /**
+     * Update is called once per frame
+     */
+    private void Update()
+    {
+        var position = transform.position;
+        var moveOffset = panSpeed * Time.deltaTime; // Speed respective of zoom
+
+        // Check for key presses
+        if (Input.GetKey("w"))
+        {
+            position.z += moveOffset;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            position.x -= moveOffset;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            position.z -= moveOffset;
+        }
+        
+        if (Input.GetKey("d"))
+        {
+            position.x += moveOffset;
+        }
+
+
+        // Update position
+        transform.position = position;
+    }
+}
