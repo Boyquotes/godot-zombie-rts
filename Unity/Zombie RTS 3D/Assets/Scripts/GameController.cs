240f77c7c24b0a8eb2c9f5247ab77bcf0778ef01
Robert Hardy <helloarchy@gmail.com>
2020-07-13 20:53:47 +0100

    Trying to have a basic unit navigation and failing on path finding

---
+++ Unity/Zombie RTS 3D/Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+
+/**
+ * Handle the overall game mechanics...
+ */
+public class GameController : MonoBehaviour
+{
+    private Vector3 _startPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Where '0' is the left button
+        }
+    }
+}
